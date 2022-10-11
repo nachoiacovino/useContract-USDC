4ea8d20c34757fd9ad6e5c93407c6277c2019e29
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-10-11 17:52:50 +0200

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import { useContract } from "@thirdweb-dev/react";
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+import ABI from "../abis/test.json";
+
+const Home: NextPage = () => {
+  const { contract: usdcContract } = useContract("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", ABI);
+  console.log(usdcContract);
+  
+  return (
+    <div className={styles.container}>
+      hi
+    </div>
+  )
+};
+
+export default Home;
